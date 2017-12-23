7eccf900ace13dbb080aaf171ec879e474d06959
HetBenkt <HetBenkt@gmail.com>
2017-12-23 13:52:07 +0100

    Some Sprinf data query samples pom.xml build changes so a non-fat jar is also created

--- src/main/java/nl/bos/gtd_server/repositories/IMemberRepository.java
+++ src/main/java/nl/bos/gtd_server/repositories/IMemberRepository.java
@@ -2,6 +2,16 @@ package nl.bos.gtd_server.repositories;
 
 import nl.bos.gtd_server.entities.Member;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IMemberRepository extends CrudRepository<Member, Long> {
+    List<Member> findByNicknameIgnoreCaseOrderByIdAsc(@Param("nickname") String nickname);
+    List<Member> findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
+    Member findFirstByOrderByLastNameAsc();
+    Member findTopByOrderByNicknameDesc();
+    List<Member> findFirst10ByLastName(@Param("lastName") String lastName);
+    List<Member> findTop10ByLastName(@Param("lastName") String lastName);
+    List<Member> findByNicknameStartingWith(@Param("nickname") String nickname);
 }
